68b532bb2aad40fd7ad9536e5cf9d9d035467bba
olayenikanmichael-Dev <olayenikanmichaelolaniyi@gmail.com>
2026-01-30 13:06:56 +0100

    in progress Hero section created

--- Frontend/app/components/Navbar/Navbar.tsx
+++ Frontend/app/components/Navbar/Navbar.tsx
@@ -1,10 +1,15 @@
 import Link from "next/link";
 import Image from "next/image";
+import SearchFilters from "@/app/components/Navbar/SearchFliters"
+import Usernav from "./Usernav";
+import AddPropertybutton from "./AddPropertybutton";
+
+
 
 const Navbar = () => {
   return (
     <nav className="w-full fixed top-0 left-0 py-0 border-b bg-white z-10">
-      <div className="max-w-[1500px] mx-auto px-6">
+      <div className="max-w-[1400px] mx-auto px-6">
         <div className="flex justify-between items-center">
           <Link href="/">
             <Image
@@ -15,8 +20,15 @@ const Navbar = () => {
               priority
             />
           </Link>
-          <div className="flex space-x-6"></div>
+          <div className="flex space-x-6">
 
+            <SearchFilters/>  
+          </div>
+<div className="flex iytems-center space-x-6">
+  <AddPropertybutton/> 
+
+  <Usernav/>
+</div>
 
 
         </div>
